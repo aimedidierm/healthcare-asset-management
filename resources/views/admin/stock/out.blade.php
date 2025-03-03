79809365e1e54f63ead558dd93d1fc6ce754068e
Aime Didier MUGISHA <aimedidiermugisha@gmail.com>
2025-03-03 09:47:16 +0200

    Stock movements UI

--- resources/views/admin/stock/out.blade.php
+++ resources/views/admin/stock/out.blade.php
@@ -35,7 +35,7 @@
             <tbody>
                 @if ($movements->isEmpty())
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" colspan="4"
+                    <th scope="row" colspan="6"
                         class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         No data available
                     </th>
